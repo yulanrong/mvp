aefc0a761133065d68023687e87d29f95f9b0a20
yulanrong <yulanrong123@gmail.com>
2022-01-25 13:50:27 -0800

    setup

---
+++ client/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+
+
+const App = () => {
+
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require("path");
+
+module.exports = {
+  mode: 'development',
+  entry: './client/src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'client/dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+}
